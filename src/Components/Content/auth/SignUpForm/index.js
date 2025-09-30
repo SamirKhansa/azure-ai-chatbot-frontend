932f13e619f22407a3e57fc723cc7c863dc19b6a
SamirKhansa <samirkhansa01@gmail.com>
2025-09-30 21:51:21 +0300

    Cleanning the code for the signup page so that we only see the html code

--- src/Components/Content/auth/SignUpForm/index.js
+++ src/Components/Content/auth/SignUpForm/index.js
@@ -3,29 +3,47 @@ import Input from "../../../Shared/Input";
 import { useState } from "react";
 import Switch from "../../../Shared/LoginRegisterSwitch";
 import axios from "axios";
+import FileButton from "../../../Shared/Button/FileButton";
 import { useNavigate } from "react-router-dom";
 const SignUpForm = ({ toggle }) => {
+    
     const navigate = useNavigate();
-    const [email, setEmail]=useState("");
-    const [Fullname, setFullName]=useState("");
-    const [Password, setPassowrd]=useState("");
-    const [ConfirmPassowrd, setConfirmPassword]=useState("");
+    const [attributes, setAttrbutes]=useState({
+      email: "",
+      Fullname: "",
+      Password:"",
+      ConfirmPassowrd:"",
+    });
+    
+    
   return (
     <div className="container">
           <div className="box">
             <h1 className="title">Register</h1>
             
             <p>Full Name</p>
-            <Input name="username" hint="eg. John Doe" icon="user" type="text" onChangeListener={(e) => setFullName(e.target.value)}
+            <Input name="Fullname" hint="eg. John Doe" icon="user" type="text" onChangeListener={(e) => setAttrbutes((prev)=>({
+              ...prev,
+              [e.target.name]: e.target.value
+            }))}
             />
             <p>Email</p>
-            <Input name="username" hint="eg. John Doe" icon="user" type="text" onChangeListener={(e) => setEmail(e.target.value)}
+            <Input name="email" hint="eg. John Doe" icon="user" type="text" onChangeListener={(e) => setAttrbutes((prev)=>({
+              ...prev,
+              [e.target.name]: e.target.value
+            }))}
             />
             <p>Password</p>
-            <Input name="password" hint="Enter a strong password" icon="lock" type="password" onChangeListener={(e) => setPassowrd(e.target.value)}
+            <Input name="Password" hint="Enter a strong password" icon="lock" type="password" onChangeListener={(e) => setAttrbutes((prev)=>({
+              ...prev,
+              [e.target.name]: e.target.value
+            }))}
             />
             <p>Cofirm Passowrd</p>
-            <Input name="ConfirmPassword" hint="Re enter your password" icon="lock" type="password" onChangeListener={(e) => setConfirmPassword(e.target.value)}
+            <Input name="ConfirmPassowrd" hint="Re enter your password" icon="lock" type="password" onChangeListener={(e) => setAttrbutes((prev)=>({
+              ...prev,
+              [e.target.name]: e.target.value
+            }))}
             />
     
             <div className="options">
@@ -34,30 +52,17 @@ const SignUpForm = ({ toggle }) => {
               </label>
               <span className="forgot-password">Forgot password?</span>
             </div>
-    
-            <Button OutsideClass={"button-container"} InsideClass={"primary-button"}
-              text="SignUp"
-              onClickListener={async() => {
-                try {
-                    const response = await axios.post("http://localhost:7071/api/ChatbotFunction/SignUp", {
-                      name: Fullname,
-                      email: email,
-                      password: Password,
-                      password_confirmation: ConfirmPassowrd,
-                      role: "User"
-                    });
 
-                    console.log("Registration successful:", response.data);
-                    navigate("/Auth"); 
 
-                  } catch (error) {
-                    console.error("Registration error:", error.response?.data || error.message);
-                   
-                  }
-
-                
-              }}
+            
+            <FileButton
+            attribute={attributes}
+            Purpose={"SignUp"}
+            text={"SignUp"}
+            InsideClass={"primary-button"}
+            OutsideClass={"button-container"}
             />
+            
             <Switch Text={"Already have an account?"} PageName={"LogIn"} toggle={toggle}/>
     
             
