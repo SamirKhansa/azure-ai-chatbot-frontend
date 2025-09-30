bd29a721f39518af21c56ed7782b788c8d9a0e2f
SamirKhansa <samirkhansa01@gmail.com>
2025-09-30 21:45:10 +0300

    Deletting a folder for storage using this Function Delete Document

--- src/Services/FileDelete.js
+++ src/Services/FileDelete.js
@@ -1,18 +1,20 @@
 import axios from "axios";
 
-export const DeleteDocument= async(resource)=>{
-    if (!resource) {
-        alert("No resource provided for deletion!");
+export const DeleteDocument= async(resource, documentName)=>{
+    if (!resource && !documentName) {
+        alert("No resource or document name provided for deletion!");
         return;
     }
 
     const response = await axios.post(
         "http://localhost:7071/api/ChatbotFunction/DeleteDocument",
-        { resource },
+        { resource:resource,
+          DocumentName: documentName
+         },
         { headers: { "Content-Type": "application/json" } }
     );
 
     console.log("Delete response:", response.data);
-    alert(`Deleted ${response.data.deleted_count} documents for resource: ${resource}`);
+    
 
 }
